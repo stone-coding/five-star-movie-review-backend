4d2109cb5ac6480641fb9f4c81782de275f6b611
stone-coding <hxs649@case.edu>
2023-07-20 19:51:26 -0400

    changed email sender

--- utils/mail.js
+++ utils/mail.js
@@ -1,3 +1,4 @@
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 const nodemailer = require("nodemailer");
 
 exports.generateOTP = (OTP_length = 6) => {
@@ -18,3 +19,24 @@ exports.generateMailTransporter = () =>
       pass: process.env.MAIL_TRAP_PASS,
     },
   });
+
+exports.sendEmail = async (name, email, subject, htmlContent) => {
+  const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+  const apiKey = defaultClient.authentications["api-key"];
+  apiKey.apiKey = process.env.SIB_API_KEY;
+
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+  const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+  sendSmtpEmail.subject = subject;
+  sendSmtpEmail.htmlContent = htmlContent;
+  sendSmtpEmail.sender = {
+    name: "Five Star MRP",
+    email: process.env.OFFICIAL_EMAIL,
+  };
+  sendSmtpEmail.to = [{ email, name }];
+
+  return await apiInstance.sendTransacEmail(sendSmtpEmail)
+};
